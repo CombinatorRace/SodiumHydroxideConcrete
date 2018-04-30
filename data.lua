da7914fbe43349ade9780881d88eb8951c53a368
combinatorMasterRace <CombinatorMasterRace@gmail.com>
2018-04-30 03:59:27 -0500

    Finished mod

--- data.lua
+++ data.lua
@@ -1,3 +1,18 @@
 data:extend({
-  
+  {
+    type = "recipe",
+    name = "concrete",
+    category = "crafting-with-fluid",
+	  subgroup = "petrochem-basics",
+    energy_required = 2,
+	  enabled = false,
+    ingredients = {
+		  {type = "fluid", name = "water", amount=100},
+      {type = "item", name = "stone", amount = 5},
+      {type = "item", name = "solid-sodium-hydroxide", amount = 2}
+	  },
+    results= {
+		  {type="item", name="concrete", amount=2}
+    },
+	},
 }
